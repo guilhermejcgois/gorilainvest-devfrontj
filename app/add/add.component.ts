7e5f21f169a88f624ce5c0e20f1b70b052200278
guilhermejcgois <guijocargo@gmail.com>
2016-12-05 02:04:56 -0200

    Adding add template

---
+++ app/add/add.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Http }      from '@angular/http';
+import { Router }    from '@angular/router';
+
+import { AngularFire } from 'angularfire2';
+
+@Component({
+  moduleId: module.id
+  , selector: 'add'
+  , templateUrl: 'add.component.html'
+  ,
+})
+export class AddComponent  {
+  constructor(
+    public router: Router
+    , public http: Http
+    , public   af: AngularFire
+  ) {}
+}
