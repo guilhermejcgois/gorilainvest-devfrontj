091285b1fffd47c9726483416ee73b9f7f33b00f
guilhermejcgois <guijocargo@gmail.com>
2016-12-06 07:51:39 -0200

    Fatality

--- dist/out-tsc/app/model/item.js
+++
@@ -1,6 +0,0 @@
-export var Item = (function () {
-    function Item() {
-    }
-    return Item;
-}());
-//# sourceMappingURL=../../../../src/app/model/item.js.map
--- dist/out-tsc/app/model/user.js
+++
@@ -1,6 +0,0 @@
-export var User = (function () {
-    function User() {
-    }
-    return User;
-}());
-//# sourceMappingURL=../../../../src/app/model/user.js.map
