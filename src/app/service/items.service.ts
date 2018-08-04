4b753b8ba9420004cfc928318e889bbf1ab19522
Guilherme Gois <guijocargo@gmail.com>
2018-08-04 20:23:12 -0300

    Fix unique lint issue on ItemsService

--- src/app/service/items.service.ts
+++ src/app/service/items.service.ts
@@ -1,6 +1,6 @@
 import { Injectable }  from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 import { Item } from '../model/item';
 
